a9748f6dc0e6f6ac2f8aaab13bfe447400135c92
Thirdy Gayares <gayaresthird@gmail.com>
2023-10-25 17:24:10 -0700

    Initial commit

---
+++ app/src/main/java/com/nineby3/aistilo/Authentication/SignUp.java
@@ -0,0 +1,14 @@
+package com.nineby3.aistilo.Authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.nineby3.aistilo.R;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
---
+++ app/src/main/java/com/nineby3/aistilo/Fragment/ScanQrCode.java
@@ -0,0 +1,14 @@
+package com.nineby3.aistilo.Fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.nineby3.aistilo.R;
+
+public class ScanQrCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_qr_code);
+    }
+}
